0c21384f125798b997cf433688a6476e9c64a2d1
Natalia <nivanova86@bk.ru>
2023-07-10 19:47:55 +0300

    add task3

--- HW2/task3.java
+++ HW2/task3.java
@@ -1,5 +1,52 @@
+//Дан следующий код, исправьте его там, где требуется
+
 package Exceptions_HW.HW2;
 
 public class task3 {
+    public static void main(String[] args) throws Exception {
+        int a = 90;
+        int b = 3;
+        if (b != 0) {
+            System.out.println(a / b);
+        } else {
+            System.out.println("Incorrect value: Division by zero");
+        }
+        
+        printSum(23, 234);
+        
+        int[] abc = { 1, 2 };
+
+        int i = 3;
+        if (i < abc.length) {
+            abc[i] = 9;
+        } else {
+            System.out.println("Incorrect index");
+        }
+    }
+
+        public static void printSum(Integer a, Integer b) {
+        System.out.println(a + b);
+        }
+
     
 }
+
+// public static void main(String[] args) throws Exception {
+//    try {
+//        int a = 90;
+//        int b = 3;
+//        System.out.println(a / b);
+//        printSum(23, 234);
+//        int[] abc = { 1, 2 };
+//        abc[3] = 9;
+//    } catch (Throwable ex) {
+//        System.out.println("Что-то пошло не так...");
+//    } catch (NullPointerException ex) {
+//        System.out.println("Указатель не может указывать на null!");
+//    } catch (IndexOutOfBoundsException ex) {
+//        System.out.println("Массив выходит за пределы своего размера!");
+//    }
+// }
+// public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+//    System.out.println(a + b);
+// }
